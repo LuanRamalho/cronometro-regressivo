1ac0957533045888f28d2968cf5dd60907c9b362
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 14:22:31 -0300

    Add files via upload

---
+++ Cronômetro Temporizador.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+from datetime import datetime, timedelta
+from sys import stdout
+from time import sleep
+
+print(' Cronometro regressivo | programador => mmxm')
+segundos = int(input('Digite a quantidade de segundos: '))
+tempo = timedelta(seconds=segundos)
+print('\n')
+
+while (str(tempo) != '0:00:00'):
+    stdout.write("\r%s"%tempo)
+    stdout.flush()
+    tempo = tempo - timedelta(seconds=1)
+    sleep(1)
+
+stdout.write("\r0:00:00")
+stdout.flush()
